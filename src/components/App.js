46f065b5225d8127614931752060a06485cc7fd1
Joseph Pearce <joseph.pearce05@gmail.com>
2020-09-11 12:21:43 -0700

    add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 function App() {
   const tap1 = "Stout";
   const tap2 = "Lager";
   return (
     <React.Fragment>
+      <Header />
       <h1>Tap Room </h1>
       <h3>{tap1}</h3>
       <h3>{tap2}</h3>
