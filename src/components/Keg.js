49740d2df3f21883129b170ee076e540efbaad24
Joseph Pearce <joseph.pearce05@gmail.com>
2020-09-11 15:22:00 -0700

    add props as argument to Keg

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,18 +1,25 @@
 import React from "react";
 
-const tap1 = "Stout";
-const tap2 = "Lager";
 
 function Keg() {
   return (
     <React.Fragment>
        <h1>Keg List </h1>
-      <h4>{tap1}</h4>
-      <h4>{tap2}</h4>
+       <h4>{props.name}</h4>
+       <h4>{props.brand}</h4>
+       <h4>{props.price}</h4>
+       <h4>{props.abv}</h4>
+      </hr>
     </React.Fragment>
   );
 
 }
 
 export default Keg;
+
+
+      // name="Stout"
+      // brand="New Belgium"
+      // price="$ 4.00"
+      // abv="8%"
 
