e29d258d78f1f512fecd9a65fe6b836d534ccd55
Joseph Pearce <joseph.pearce05@gmail.com>
2020-09-11 11:31:07 -0700

    add variables in curly braces - jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 function App() {
+  const tap1 = "Stout";
+  const tap2 = "Lager";
   return (
     <React.Fragment>
       <h1>Tap Room </h1>
-
+      <h3>{tap1}</h3>
+      <h3>{tap2}</h3>
     </React.Fragment>
   );
 }
